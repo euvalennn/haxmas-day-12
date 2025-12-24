f1290c4e0f757777f8d702483b19c591f60c0cd2
euvalennn <97848208+euvalennn@users.noreply.github.com>
2025-12-24 19:50:29 -0300

    finished site wooo!!

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
+import "./style.css";
 import * as THREE from "three";
 
-const scene = new THREE.scene();
+const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
   75,
   window.innerWidth / window.innerHeight,
@@ -8,24 +9,64 @@ const camera = new THREE.PerspectiveCamera(
   1000
 );
 
-const renderer = new THREE.WebGLRenderer();
+const renderer = new THREE.WebGLRenderer({
+  canvas: document.querySelector("#bg"),
+});
+
 renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setAnimationLoop(animate);
+renderer.render(scene, camera);
+
 document.body.appendChild(renderer.domElement);
 
 const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const texture = new THREE.TextureLoader().load("heidi.png");
+const material = new THREE.MeshBasicMaterial({ map: texture });
 const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 
-camera.position.z = 5;
+const donut_geo = new THREE.TorusGeometry(10, 3, 16, 100);
+const donut_tex = new THREE.TextureLoader().load("orpheus.png");
+const donut_mat = new THREE.MeshBasicMaterial({ map: donut_tex });
+const donut = new THREE.Mesh(donut_geo, donut_mat);
+scene.add(donut);
+
+camera.position.z = 18;
 
 function animate() {
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
+  donut.rotation.x += 0.01;
+  donut.rotation.y += 0.01;
 
   renderer.render(scene, camera);
-
-  requestAnimationFrame(animate);
 }
 
-animate();
+function add_star() {
+  const star_geometry = new THREE.SphereGeometry(0.25, 24, 24);
+  const star_material = new THREE.MeshBasicMaterial({ color: 0xffffff });
+  const star = new THREE.Mesh(star_geometry, star_material);
+
+  const [x, y, z] = Array(3)
+    .fill()
+    .map(() => THREE.MathUtils.randFloatSpread(200));
+
+  star.position.set(x, y, z);
+  scene.add(star);
+}
+
+Array(200).fill().forEach(add_star);
+
+camera.position.setZ(45);
+
+function moveCamera() {
+  const t = document.body.getBoundingClientRect().top;
+
+  cube.rotation.y += 0.01;
+  cube.rotation.z += 0.01;
+
+  camera.position.z = t * -0.01;
+  camera.position.x = t * -0.0;
+  camera.rotation.y = t * -0.0;
+}
+
+document.body.onscroll = moveCamera;
+moveCamera();
